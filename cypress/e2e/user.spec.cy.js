6ac14b19aca2ad791bf446fff03eda078df30db1
iv4n-ssf <ivanildo.ssf@gmail.com>
2026-01-30 10:58:49 -0300

    Adicionando firs name, midde name e last name

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,20 +8,35 @@ describe('Orange HRM Tests', () => {
     loginButton: ".oxd-button",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert"
+    wrongCredentialAlert: ".oxd-alert",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firsNameField: "[name='firstName']",
+    middleName: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    // CAMPO GENERICO
+    genericField: ".oxd-input--active",
   }
 
-  it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  it.only('User Info Update - Success', () => {
+
+    // LOGIN
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userDate.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userDate.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    // MY INFO
+    //cy.visit('/pim/viewMyDetails')
+    // cy.get("[href='/web/index.php/pim/viewMyDetails']").click()
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firsNameField).type('First Name Test')
+    cy.get(selectorsList.middleName).type('Middle Name Test')
+    cy.get(selectorsList.lastNameField).type('Last Name Test')
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userDate.userFail.username)
     cy.get(selectorsList.passwordField).type(userDate.userFail.password)
     cy.get(selectorsList.loginButton).click()
